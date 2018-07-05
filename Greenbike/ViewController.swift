e17436421cac7a7290fbcb8b700cce3990635032
Başar Noyan <basarnoyan1@gmail.com>
2018-07-05 20:32:00 +0300

    Add files via upload

---
+++ Greenbike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Greenbike
+//
+//  Created by Başar on 5.07.2018.
+//  Copyright (c) 2018 evall.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
