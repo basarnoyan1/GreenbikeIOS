796abffe862d0d813e203df997a7b279cb6e49a0
Başar Noyan <basarnoyan1@gmail.com>
2018-08-13 13:36:23 +0300

    Xcode 1

--- Greenbike/ViewController.swift
+++ Greenbike/ViewController.swift
@@ -1,25 +1,47 @@
-//
-//  ViewController.swift
-//  Greenbike
-//
-//  Created by Başar on 5.07.2018.
-//  Copyright (c) 2018 evall.io. All rights reserved.
-//
-
+import CoreBluetooth
 import UIKit
 
 class ViewController: UIViewController {
-
+    var centralManager: CBCentralManager?
+    var peripherals = Array<CBPeripheral>()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Initialise CoreBluetooth Central Manager
+        centralManager = CBCentralManager(delegate: self, queue: DispatchQueue.main)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+extension ViewController: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if (central.state == .poweredOn){
+            self.centralManager?.scanForPeripherals(withServices: nil, options: nil)
+        }
+        else {
+            // do something like alert the user that ble is not on
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        peripherals.append(peripheral)
+        tableView.reloadData()
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "cell")! as UITableViewCell
+        
+        let peripheral = peripherals[indexPath.row]
+        cell.textLabel?.text = peripheral.name
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return peripherals.count
+    }
+}
